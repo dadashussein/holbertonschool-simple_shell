711072a91f0db877fb65a30ace08d676c581b4c1
aytachuseynli <aytac_ha@mail.ru>
2023-12-26 12:40:03 -0800

    env

--- execute_command.c
+++ execute_command.c
@@ -1,10 +1,7 @@
 #include "shell.h"
 
-<<<<<<< HEAD
 #define UNUSED(x) (void)(x)
 
-=======
->>>>>>> 07415775e4a4a590b920588709098b85a63aea3c
 /**
  * execute_command_path - Execute a command with a given path.
  *
@@ -15,7 +12,6 @@
  */
 void execute_command_path(char **args, char *path)
 {
-<<<<<<< HEAD
     /* ... (unchanged code) */
 
     /* If the loop completes, the command was not found in any path */
@@ -40,37 +36,6 @@ void print_environment(void)
         printf("%s\n", environ[i]);
         i++;
     }
-=======
-	char *token = strtok(path, ":");
-	char *full_path;
-
-	while (token != NULL)
-	{
-		full_path = malloc(strlen(token) + strlen(args[0]) + 2);
-
-		if (full_path == NULL)
-		{
-			perror("Error:");
-			exit(EXIT_FAILURE);
-		}
-
-		strcpy(full_path, token);
-		strcat(full_path, "/");
-		strcat(full_path, args[0]);
-
-		if (execve(full_path, args, NULL) != -1)
-		{
-			free(full_path);
-			exit(EXIT_SUCCESS);
-		}
-
-		free(full_path);
-		token = strtok(NULL, ":");
-	}
-
-	fprintf(stderr, "Command not found: %s\n", args[0]);
-	exit(EXIT_FAILURE);
->>>>>>> 07415775e4a4a590b920588709098b85a63aea3c
 }
 
 /**
@@ -82,12 +47,11 @@ void print_environment(void)
  */
 void execute_command(char **args)
 {
-<<<<<<< HEAD
     pid_t child_pid;
     int status;
     char *path;
 
-    /* Check if the command is the built-in "exit" */
+    /* Check if the command is a built-in "exit" */
     if (strcmp(args[0], "exit") == 0)
     {
         /* Exit the shell if "exit" is entered */
@@ -95,7 +59,7 @@ void execute_command(char **args)
     }
     else if (strcmp(args[0], "env") == 0)
     {
-        /* If the command is "env", print the environment */
+        /* Print the environment if "env" is entered */
         print_environment();
         return;
     }
@@ -109,6 +73,7 @@ void execute_command(char **args)
     }
     else if (child_pid == 0)
     {
+        /* Child process */
         if (strchr(args[0], '/') != NULL)
         {
             /* If the command includes a '/', use execve directly */
@@ -127,6 +92,7 @@ void execute_command(char **args)
     }
     else
     {
+        /* Parent process */
         /* In the parent process, wait for the child */
         if (waitpid(child_pid, &status, 0) == -1)
         {
@@ -134,32 +100,4 @@ void execute_command(char **args)
             exit(EXIT_FAILURE);
         }
     }
-=======
-	pid_t child_pid;
-	int status;
-	char *path;
-
-	child_pid = fork();
-
-	if (child_pid == -1)
-		perror("fork");
-	else if (child_pid == 0)
-	{
-		if (strchr(args[0], '/') != NULL)
-		{
-			if (execve(args[0], args, NULL) == -1)
-			{
-				perror("Error:");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			path = getenv("PATH");
-			execute_command_path(args, path);
-		}
-	}
-	else
-		wait(&status);
->>>>>>> 07415775e4a4a590b920588709098b85a63aea3c
 }
